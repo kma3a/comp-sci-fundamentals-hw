f8c396ded328f8e5ce9c71da9f57eeb91c052958
Kelsey Ando <kelsey.ando@detroitlabs.com>
2018-02-05 18:48:17 -0500

    adding the file for hw3

---
+++ session2/hw3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void sort(int a[], int n) {
+  int i,j, temp;
+
+  for(i=0; i<n-1; ++i) {
+    for (j=i+1; j<n; ++j){
+      if( a[i] > a[j]){
+        temp=a[i];
+        a[i]=a[j];
+        a[j]=temp;
+      }
+    }
+  }
+}
+
+int main (void) {
+
+}
