e97fae1dfc3268eeb9b1dff7b95e6dbf94b52ab7
Kelsey Ando <kelsey.ando@detroitlabs.com>
2018-02-11 20:26:57 -0500

    adding hw2

--- session2/hw2.c
+++ session2/hw2.c
@@ -1,3 +1,25 @@
 #include <stdio.h>
+
+int gcd(int u , int v) {
+    int temp;
+
+    while (v != 0){
+      temp = u % v;
+      u = v;
+      v = temp;
+    }
+
+    return u;
+}
+
+int lcm ( int int1, int int2){
+
+  return int1 * int2 / gcd( int1, int2);
+
+}
+
+
 int main (void) {
+
+  printf(" the lcd if 15 and 10 is %d \n", lcm(15, 10));
 }
