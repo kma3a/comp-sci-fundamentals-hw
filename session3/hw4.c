eff556a831a7e0d2355203a0e8dee4f058e5f141
Kelsey Ando <kelsey.ando@detroitlabs.com>
2018-02-12 01:00:42 -0500

    adding homework 4 of session 3

---
+++ session3/hw4.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+int findString(char array[], char searchString[]){
+  int searchIndex = 0;
+  int searchLetter = 0;
+  for(int i = 0; array[i] != '\0' && searchString[searchLetter] != '\0'; ++i) {
+    if ( array[i] == searchString[searchLetter]) {
+      if(searchLetter == 0) {
+        searchIndex = i;
+      }
+      searchLetter++;
+    }
+  }
+  return searchIndex;
+}
+
+void removeString(char array[], int removeStart, int removeEnd){
+  for(int i = 0; array[i] != '\0'; ++i) {
+    if(array[i+removeEnd] == '\0'){
+      array[i] = '\0';
+      return;
+    }
+    if( i >= removeStart) {
+      array[i] = array[i+removeEnd];
+    }
+  }
+
+
+}
+
+void insertString(char array[], char insertChar[], int insertStart){
+  char newArray[20];
+  int currentIndex =0;
+
+  for(int i =0; array[i] != '\0'; ++i){
+
+    if(i == insertStart) {
+      for(int j=0; insertChar[j] != '\0'; ++j){
+        newArray[currentIndex] = insertChar[j];
+        ++currentIndex;
+      }
+    }
+    newArray[currentIndex] = array[i];
+    ++currentIndex;
+  }
+
+  newArray[currentIndex] = '\0';
+
+  printf("%s", newArray);
+}
+
+int getLength(char string[]) {
+  int index = 0;
+  while( string[index] != '\0') {
+    ++index;
+  }
+  return index;
+}
+
+void replaceString(char text[], char string1[], char string2[]){
+  int stringLength = getLength(string1);
+  int index = findString(text, string1);
+  removeString(text, index, stringLength);
+  insertString(text, string2, index);
+}
+
+int main(void){
+  char text[] = "I am the 1 truth";
+  replaceString(text, "1", "one");
+
+}
