b583fa43cbd98491f75943e5451c018262bf96cb
Kelsey Ando <kelsey.ando@detroitlabs.com>
2018-02-18 19:24:26 -0500

    adding hw6

---
+++ session3/hw6.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/*
+ * struct for the double link list
+ */
+struct link {
+  int value;
+  struct link *next;
+  struct link *previous;
+};
+
+void insertEntry(struct link *prior_link, struct link *addition){
+
+  addition->next = prior_link->next;
+  addition->previous = prior_link;
+
+  prior_link->next = addition;
+
+}
+
+void removeEntry(struct link *removal){
+  if( removal->next != (struct link *) 0) {
+    removal->previous->next = removal->next;
+    removal->next->previous = removal->previous;
+  } else {
+    removal->previous->next = (struct link *) 0;
+  }
+
+
+}
+
+
+
+int main(void){
+  struct link one, two, three;
+  struct link *list_pointer = &one;
+  struct link new;
+
+  one.value = 2;
+  one.next = &two;
+
+  two.previous = &one;
+  two.value = 8;
+  two.next = &three;
+
+  three.previous = &two;
+  three.value = 66;
+  three.next = (struct link *) 0;
+
+  new.value = 45;
+  
+  //insertEntry(&two, &new);
+  removeEntry(&three);
+
+  while( list_pointer != (struct link *) 0) {
+    printf("%i \n", list_pointer->value);
+    list_pointer = list_pointer->next;
+  }
+  
+  return 0;
+}
