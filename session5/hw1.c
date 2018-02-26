d603f7e6d3ee55b5bde6b73efa90d681d3abaa99
Kelsey Ando <kelsey.ando@detroitlabs.com>
2018-02-25 19:56:33 -0500

    adding the frst part of the remove string function to make it so we create a new string and remove the part that we don't want

--- session5/hw1.c
+++ session5/hw1.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 
+int stringLength(char *string) {
+  int index =1;
+  while(string[index] != '\0') {
+    index++;
+  }
+
+  return index;
+}
+
+void removeString(char *string, int startIndex, int numRemoved){
+  char finishedString[stringLength(string) - numRemoved];
+  int index = 0, finishedIndex = 0;
+  for(; string[index] != '\0'; index++) {
+    if(index < startIndex || index > ( startIndex + numRemoved )) {
+      finishedString[finishedIndex] = string[index];
+      finishedIndex++;
+    }
+  }
+  finishedString[finishedIndex] = '\0';
+  printf("%s \n", finishedString);
+
+}
+
 int main (void) {
+  removeString("I am your father", 4, 4);
   return 0;
 }
