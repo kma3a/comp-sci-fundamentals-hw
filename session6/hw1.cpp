1c511f353b6bc55ad524a550075f832619cf0f75
Kelsey Ando <kelsey.ando@detroitlabs.com>
2018-03-04 21:27:46 -0500

    adding the first assignment

---
+++ session6/hw1.cpp
@@ -0,0 +1,15 @@
+/* 
+ * Design a C++ sructure to store a diary product name, portion weight, calories, protein, fat, and carbohydrates,
+ *
+ * 25 grams of American cheese have:
+ * 375 Calories
+ * 5 grams of protein
+ * 8 grams of fat
+ * 0 carbohydrates
+ *
+ * show how to assign these values to the member variables of your structure.
+ * Write a function that given a variable of type struct dairy and a weight in grams (protion size) returns the number of calories for that weight.
+ */
+
+#include <iostream>
+#include <string>
